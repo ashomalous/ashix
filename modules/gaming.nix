da16a76b416ae17078eb7ed2b6a3160d008f74ed
Ashomaly <ashomalous@gmail.com>
2026-07-08 20:39:54 -0500

    refactor steam and add persistence

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -1,52 +1,60 @@
-{
-  den.aspects.gaming.nixos =
-    { lib, pkgs, ... }:
-    {
-      programs.steam = {
-        enable = true;
-        remotePlay.openFirewall = true;
-        dedicatedServer.openFirewall = true;
-        gamescopeSession.enable = true;
-      };
+{ den, ... }: {
+  den.aspects.gaming = {
+    includes = with den.aspects; [ steam ];
 
-      programs.gamemode = {
-        enable = true;
-        settings = {
-          general = {
-            renice = 10;
-          };
+    nixos =
+      { lib, pkgs, ... }:
+      {
+        programs.gamemode = {
+          enable = true;
+          settings = {
+            general = {
+              renice = 10;
+            };
 
-          # Warning: GPU optimisations have the potential to damage hardware
-          gpu = {
-            apply_gpu_optimisations = "accept-responsibility";
-            gpu_device = 0;
-          };
+            # Warning: GPU optimisations have the potential to damage hardware
+            gpu = {
+              apply_gpu_optimisations = "accept-responsibility";
+              gpu_device = 0;
+            };
 
-          custom = {
-            start = "${lib.getExe' pkgs.libnotify "notify-send"} 'GameMode started'";
-            end = "${lib.getExe' pkgs.libnotify "notify-send"} 'GameMode ended'";
+            custom = {
+              start = "${lib.getExe' pkgs.libnotify "notify-send"} 'GameMode started'";
+              end = "${lib.getExe' pkgs.libnotify "notify-send"} 'GameMode ended'";
+            };
           };
         };
+
+        hardware.graphics.enable32Bit = true;
+
+        environment.systemPackages = with pkgs; [
+          lutris
+          umu-launcher
+          faugus-launcher
+
+          protonup-qt
+          winetricks
+
+          mangohud
+          gamemode
+
+          sc-controller
+
+          nvtopPackages.full
+        ];
+
+        persist.user.directories = [ "Games" ];
       };
+  };
 
-      hardware.graphics.enable32Bit = true;
-
-      environment.systemPackages = with pkgs; [
-        lutris
-        umu-launcher
-        faugus-launcher
-
-        protonup-qt
-        winetricks
-
-        mangohud
-        gamemode
-
-        sc-controller
-
-        nvtopPackages.full
-      ];
-
-      persist.user.directories = [ "Games" ];
+  den.aspects.steam.nixos = _: {
+    programs.steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      gamescopeSession.enable = true;
     };
+
+    persist.user.directories = [ ".local/share/Steam" ];
+  };
 }
